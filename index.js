439cb2bc5a657330232c9a2bfd7ea529ff3c277b
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-05-02 08:38:23 +0600

    recipes added for two chef

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const app = express();
 const port = process.env.port || 5000;
 
 const chef = require('./data/chef.json');
-
+const recipes = require('./data/recipes.json')
 app.get('/', (req, res)=>{
     res.send(chef);
 })
 
-
+app.get('/recipes', (req, res)=>{
+    res.send(recipes);
+})
 
 
 
